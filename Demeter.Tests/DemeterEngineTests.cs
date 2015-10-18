a7f0f486853b69a4fe336962be0e40ab4847a114
Emmanuel Ernest <ernest.emmanuel@hotmail.fr>
2015-10-18 17:41:17 +0200

    Handle moisture level. low = 0 to 50. high 50 to 100

--- Demeter.Tests/DemeterEngineTests.cs
+++ Demeter.Tests/DemeterEngineTests.cs
@@ -10,6 +10,21 @@ namespace Demeter.Tests
 {
     public class DemeterEngineTests
     {
-        
+        [TestCase(10,DemeterEngine.MoistureLevel.Low)]
+        [TestCase(20, DemeterEngine.MoistureLevel.Low)]
+        [TestCase(40, DemeterEngine.MoistureLevel.Low)]
+        [TestCase(55, DemeterEngine.MoistureLevel.High)]
+        [TestCase(80, DemeterEngine.MoistureLevel.High)]
+        [TestCase(100, DemeterEngine.MoistureLevel.High)]
+        public void EngineShouldReturnMoistureLevel(int actualMoistureInPercentage, DemeterEngine.MoistureLevel level)
+        {
+            var moistureSensor = Substitute.For<IMoistureSensor>();
+            moistureSensor.GetLevelInPourcentage().Returns(actualMoistureInPercentage);
+            var engine = new DemeterEngine(moistureSensor);
+
+            var moistureLevel = engine.GetMoistureLevel();
+            moistureSensor.Received().GetLevelInPourcentage();
+            Assert.AreEqual(moistureLevel, level);
+        }
     }
 }
--- Demeter/DemeterEngine.cs
+++ Demeter/DemeterEngine.cs
@@ -8,6 +8,29 @@ namespace Demeter
 {
     public class DemeterEngine
     {
+        private readonly IMoistureSensor _moistureSensor;
 
+        public DemeterEngine( IMoistureSensor moistureSensor)
+        {
+            _moistureSensor = moistureSensor;
+        }
+        public MoistureLevel GetMoistureLevel()
+        {
+            var level = _moistureSensor.GetLevelInPourcentage();
+            if (level < 50)
+            {
+                return MoistureLevel.Low;
+            }
+            return MoistureLevel.High;
+        }
+
+        public enum MoistureLevel
+        {
+            VeryLow,
+            Low,
+            High,
+            VeryHigh
+
+        }
     }
 }
